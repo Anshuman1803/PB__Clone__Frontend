b3d0c0056b1911701d82030c762bed61c894a359
Anshuman Kumar <mahianshuman491@gmail.com>
2023-12-09 23:24:18 +0530

    mock text header part completed rest pending

--- src/UI-Components/MockTestComponent.js
+++ src/UI-Components/MockTestComponent.js
@@ -2,9 +2,20 @@ import React from 'react'
 
 function MockTestComponent() {
   return (
-    <div>
+    <>
+      <section className='MockTestComponent__MainContainer'>
+
+        <div className="MockComponent__header">
+          <h2 className='MockComponent__header_Text'>Mock Tests</h2>
+          <p className='MockComponent__header_desc'>Technical and Aptitude Test is a very important process of most of the placement tests. Crack your next placement with series of PrepBytes practice and mock tests. Practice subject-wise and company-wise tests. Take real-time mock tests with other students and test your preparation.</p>
+        </div>
+
+       <div className="MockTest__CardContainer">
+       <h1 className='MockTestCompo__featuredHeading'>Featured Mock Tests</h1>
+       </div>
+      </section>
       <div className='helpContainer__Message'>Need Help? Talk to us on  079 6900 2111 or Request Callback</div>
-    </div>
+    </>
   )
 }
 
